57665ad9e9f3857e6cd98c7484cc299c3f3f0ec7
Anton len <farvat24@rambler.ru>
2025-07-19 13:04:40 +0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String name = "Anton";
+        String surname = "Len";
+        System.out.print(name + " " + surname);
+    }
+}
