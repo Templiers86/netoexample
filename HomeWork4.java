fd5377b7970e104752188bafe0c91795ebdfdf0f
Anton len <farvat24@rambler.ru>
2025-07-23 21:47:57 +0500

    Add files via upload

---
+++ HomeWork4.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+import static my.netology.service.CustomsService.calculateCustoms;
+
+public class HomeWork4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Доброго времени суток");
+
+        System.out.println("Введите цену товара (руб.):");
+        int prace = scanner.nextInt();
+
+        System.out.println("Введите вес товара (в кг.):");
+        int weight = scanner.nextInt();
+
+        int duty = calculateCustoms(prace, weight);
+        System.out.println("Вывод пошлины:" + duty + "(руб.)" );
+
+    }
+
+
+}
